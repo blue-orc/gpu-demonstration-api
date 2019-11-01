d7ffe0a1363d5c2f1917e404ec669d1749e0d116
Justin Blau <justin.blau@oracle.com>
2019-11-01 11:36:29 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"gpu-demonstration-api/DeviceMonitor"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":9002", "http service address")
+var addrTLS = flag.String("addrTLS", ":9003", "https service address")
+
+func main() {
+	DeviceMonitor.Init()
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
