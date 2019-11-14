151ba0af6b37bf61fd490b2cd618302b17ff668a
Justin Blau <justin.blau@oracle.com>
2019-11-14 15:37:01 -0500

    sql bugs

--- database/repositories/battery.go
+++ database/repositories/battery.go
@@ -22,9 +22,12 @@ func (br *BatteryRepo) SelectSampleDischargeTop(top int) ([]models.BatteryDischa
 		FROM battery_battery 
 		LEFT JOIN battery_cycle ON battery_battery.battery_id = battery_cycle.battery_id
 		LEFT JOIN battery_discharge_data ON battery_discharge_data.cycle_id = battery_cycle.cycle_id
-		WHERE ROWNUM <= $1`,
+		WHERE ROWNUM <= :1`,
 		top,
 	)
+	if err != nil {
+		return ds, err
+	}
 
 	defer rows.Close()
 	for rows.Next() {
@@ -34,6 +37,7 @@ func (br *BatteryRepo) SelectSampleDischargeTop(top int) ([]models.BatteryDischa
 			&d.CurrentMeasured,
 			&d.TemperatureMeasured,
 			&d.VoltageLoad,
+			&d.VoltageMeasured,
 			&d.Time,
 			&d.Capacity,
 			&d.PercentRemainingUsefulLife,
