0995558257a5a81fe58daef5d695a1d607fbf929
Justin Blau <justin.blau@oracle.com>
2019-12-02 14:59:46 -0500

    tensordataset

--- python-job-runner/scripts/citibike_station_arrivals_by_day_gpu.py
+++ python-job-runner/scripts/citibike_station_arrivals_by_day_gpu.py
@@ -104,7 +104,7 @@ def main(argv):
     y_tensor = torch.Tensor(y_norm)
     y_ok = y_tensor.unsqueeze(1).to(device)
 
-    trainds = torch.utils.data.TorchDataset(x_tensor, y_ok)
+    trainds = torch.utils.data.TensorDataset(x_tensor, y_ok)
     trainloader = torch.utils.data.DataLoader(trainds, batch_size=1, shuffle=False, num_workers=0)
 
     model = LogisticRegression(input_dim,output_dim).to(device)
