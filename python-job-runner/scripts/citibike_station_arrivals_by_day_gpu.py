9a66d8c0786469356580bc3df55a7270df4263a9
Justin Blau <justin.blau@oracle.com>
2019-12-02 15:24:36 -0500

    debug

--- python-job-runner/scripts/citibike_station_arrivals_by_day_gpu.py
+++ python-job-runner/scripts/citibike_station_arrivals_by_day_gpu.py
@@ -105,7 +105,7 @@ def main(argv):
     y_ok = y_tensor.unsqueeze(1)
 
     trainds = torch.utils.data.TensorDataset(x_tensor, y_ok)
-    trainloader = torch.utils.data.DataLoader(trainds, batch_size=32, shuffle=False, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainds, batch_size=128, shuffle=False, num_workers=10)
 
     model = LogisticRegression(input_dim,output_dim).to(device)
     criterion = torch.nn.MSELoss().to(device)# Mean Squared Loss
