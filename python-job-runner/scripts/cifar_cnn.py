ce14a8c809f9c73c973c055ddd40a39e673065e5
Justin Blau <justin.blau@oracle.com>
2020-01-21 18:38:26 -0500

    as

--- python-job-runner/scripts/cifar_cnn.py
+++ python-job-runner/scripts/cifar_cnn.py
@@ -162,25 +162,24 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 for epoch in range(100):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data[0], data[1]
+    # get the inputs; data is a list of [inputs, labels]
+    inputs, labels = trainset.data.to(device), trainset.targets.to(device)
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+    # zero the parameter gradients
+    optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    # forward + backward + optimize
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+    # print statistics
+    running_loss += loss.item()
+    if epoch % 10 == 9:    # print every 2000 mini-batches
+        print('[%d, %5d] loss: %.3f' %
+                (epoch + 1, i + 1, running_loss / 2000))
+        running_loss = 0.0
 
 print('Finished Training')
 
