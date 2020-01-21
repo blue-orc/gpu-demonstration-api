27d1488f1c02efc98ee84b79673ec6a92a068129
Justin Blau <justin.blau@oracle.com>
2020-01-21 18:30:24 -0500

    ok

--- python-job-runner/scripts/cifar_cnn.py
+++ python-job-runner/scripts/cifar_cnn.py
@@ -72,7 +72,7 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=10000,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
